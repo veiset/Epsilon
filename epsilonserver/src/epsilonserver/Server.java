78883bc6238b3dd7762a96f139151d67336add2b
magnus <magnus>
2010-09-28 22:32:09 +0000

    git-svn-id: http://www.nlogn.org/svn/epsilon/trunk@18 189e0cc2-7543-4f94-9523-152a149c9a55

---
+++ epsilonserver/src/epsilonserver/Server.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package epsilonserver;
+
+/**
+ *
+ * @author mm
+ */
+public class Server {
+
+}
