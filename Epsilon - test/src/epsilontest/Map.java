fc21341879f71ac9aca4cb5e27ca881b03e56a22
marius <marius>
2010-09-29 09:02:01 +0000

    Added a few classes with basic functionality.
    git-svn-id: http://www.nlogn.org/svn/epsilon/trunk@21 189e0cc2-7543-4f94-9523-152a149c9a55

---
+++ Epsilon - test/src/epsilontest/Map.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package epsilontest;
+
+/**
+ *
+ * @author Marius
+ */
+public class Map {
+
+}
---
+++ Epsilon - test/src/epsilontest/SpriteStore.java
@@ -0,0 +1,11 @@
+package epsilontest;
+
+/**
+ * Storing class for sprites. Implemented as a singleton.
+ * Makes sure that sprites are only loaded into memory once.
+ *
+ * @author Marius
+ */
+public class SpriteStore {
+
+}
