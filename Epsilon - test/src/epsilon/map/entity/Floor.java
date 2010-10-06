db5d878c0ba9c5aa536005ac108226e55d8f1943
veiset <veiset@gmail.com>
2010-10-06 11:43:49 +0000

    Very basic hitbox idea. RFC!
    git-svn-id: http://www.nlogn.org/svn/epsilon/trunk@47 189e0cc2-7543-4f94-9523-152a149c9a55

--- Epsilon - test/src/epsilon/map/entity/Floor.java
+++ Epsilon - test/src/epsilon/map/entity/Floor.java
@@ -8,10 +8,48 @@ import epsilon.game.Sprite;
  */
 public class Floor extends World {
 
-
     public Floor(int posX, int posY) {
-        super(posX,posY);
+        super(posX, posY);
         currentSprite = new Sprite(new String[]{"/pics/crate.png"});
     }
 
+    // Method for hitbox for this object (concept):
+    //   ______
+    //  |      |
+    //  |______|
+    // 
+    public boolean hitBox(int x, int y) {
+        // public boolean hitbox(Entity toCheckAgainst) {
+        boolean hit = true;
+
+        double left1, left2;
+        double right1, right2;
+        double top1, top2;
+        double bottom1, bottom2;
+
+        left1 = this.posX;
+        left2 = x;
+        right1 = this.posX; // (+image.width)
+        right2 = x;
+        top1 = this.posY;
+        top2 = y; // (+ offset?)
+        bottom1 = this.posY; // (+image.height)
+        bottom2 = y; // (+ offset?)
+
+        if (bottom1 < top2) {
+            hit = false;
+        }
+        if (top1 > bottom2) {
+            hit = false;
+        }
+
+        if (right1 < left2) {
+            hit = false;
+        }
+        if (left1 > right2) {
+            hit = false;
+        }
+
+        return hit;
+    }
 }
