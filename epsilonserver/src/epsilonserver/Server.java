b0062e29ebac0a9787a19c463779a33d7bab18e1
magnus <magnus>
2010-10-13 00:48:05 +0000

    Multiplayer server
    Listens for incoming UDP traffic and echos it back to the sender.
    
    Next is to expand transfer protocol and make the server support multiple clients. 
    
    git-svn-id: http://www.nlogn.org/svn/epsilon/trunk@74 189e0cc2-7543-4f94-9523-152a149c9a55

--- epsilonserver/src/epsilonserver/Server.java
+++ epsilonserver/src/epsilonserver/Server.java
@@ -1,11 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package epsilonserver;
 
+import java.io.IOException;
+
 /**
+ *
+ * Simple server that receives object state data and sends it to connected clients.
+ * Will expand to checking the validity of the received object state data.
  *
  * @author mm
  */
@@ -15,10 +16,45 @@ public class Server {
     private ServerHandler sh;
 
 
+    /**
+     *
+     * Constructor
+     *
+     * Start a server handler with a specified port
+     *
+     */
     public Server() {
-        sh = new ServerHandler();
-        new Thread(sh).start();
 
+        try {
+            sh = new ServerHandler(port);
+        }
+        catch(IOException ioe) {
+            System.out.println("Datagramsocket could not be created");
+            System.out.println(ioe.getMessage());
+        }
+
+    }
+
+
+    /**
+     *
+     * Start serverhandler thread
+     *
+     */
+    public void startServer() {
+        new Thread(sh).start();
+    }
+    
+
+    /**
+     *
+     * Programs main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Server s = new Server();
+        s.startServer();
     }
 
 
--- epsilonserver/src/epsilonserver/ServerHandler.java
+++ epsilonserver/src/epsilonserver/ServerHandler.java
@@ -1,27 +1,94 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package epsilonserver;
 
-import java.net.Socket;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 /**
+ *
+ * Server handler that handles received object state data from clients
+ * 
+ * Right now it will only echo back data received from a client
  *
  * @author mm
  */
 public class ServerHandler implements Runnable {
 
-    private Socket udpsocket;
+
+    private DatagramSocket socket = null;
+    private boolean running = true;
+//    private ArrayList<InetAddress> clientList = new ArrayList<InetAddress>();
 
 
-    public ServerHandler() {
+    /**
+     *
+     * Constructor
+     * Open a UDP socket with a specified port 
+     *
+     * @param port
+     * @throws SocketException
+     */
+    public ServerHandler(int port) throws SocketException {
+        socket = new DatagramSocket(port);
 
     }
 
+
+    /**
+     *
+     * Start a thread that listens for incoming UDP traffic. When data is
+     * received it is sendt back to the sender.
+     *
+     */
     public void run() {
-        // stuff
+
+        while (running) {
+
+            byte[] buf = new byte[256];
+
+            try {
+                DatagramPacket incoming = new DatagramPacket(buf, buf.length);
+                socket.receive(incoming);
+                System.out.println("reveived packet");
+
+                InetAddress clientaddress = incoming.getAddress();
+                int clientport = incoming.getPort();
+
+//              if (!clientList.contains(address)) {
+//                  clientList.add(address);
+//              }
+//
+//              for (int i=0; i<clientList.size(); i++) {
+//                    InetAddress a = clientList.get(i);
+//                    DatagramPacket outgoing = new DatagramPacket(buf, buf.length, a, 6001);
+//                    socket.send(outgoing);
+//                    System.out.println("packet sendt");
+//              }
+
+                DatagramPacket outgoing = new DatagramPacket(buf, buf.length, clientaddress, clientport);
+                socket.send(outgoing);
+                System.out.println("packet sendt");
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+ 
+        }
+        socket.close();
     }
+
+
+    /**
+     *
+     * Stop listening for incoming UDP traffic
+     * 
+     */
+    public void stopServer() {
+        running = false;
+    }
+
 
 }
